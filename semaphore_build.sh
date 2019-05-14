0525a8b715b6f0cbb5a21cce395eb9dcd9d7ce38
jp <jplikesbikes@gmail.com>
2019-05-14 15:07:52 -0400

    semaphore pushing to gke

--- semaphore_build.sh
+++ semaphore_build.sh
@@ -1,9 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
 set -e
 
-echo "Building image..."
 export EB_VERSION=`git rev-parse --short=7 HEAD`
 
-echo "Building server"
-docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$EB_VERSION .
+echo "Building image..."
+docker build --no-cache \
+	--build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+	--build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+	-t gcr.io/datavore-dev/$IMAGE_REPO:$EB_VERSION \
+	-t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$EB_VERSION .
--- semaphore_deploy.sh
+++ semaphore_deploy.sh
@@ -7,3 +7,5 @@ export EB_VERSION=`git rev-parse --short=7 HEAD`
 
 $(aws ecr get-login --region us-east-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$EB_VERSION
+docker push gcr.io/datavore-dev/$IMAGE_REPO:$EB_VERSION
+
